8779a0e781f59dcee306d408cb4bf1d39acdbb0a
alexcarantona <info@alejandrocarantonna.es>
2021-12-25 02:54:07 +0100

    #3.10 Minor tweaks to deploy app backend to Heroku
    Changes port and adds Procfile

--- index.js
+++ index.js
@@ -72,5 +72,5 @@ app.get('/info', (req, res) => {
     <p>Request made at ${now}</p>`)
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`))
