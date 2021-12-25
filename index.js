000b78be5db51332ea3ed6d3d49af3a27aa458b5
alexcarantona <info@alejandrocarantonna.es>
2021-12-25 03:21:56 +0100

    #3.11 Fullstack and build
    Frontend build integrated with backend

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ morgan.token('data', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :data'));
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 
 var persons = [
     {
