c8d6d4eaff0e7b3c1727f7ce0290c15e9cd361ce
alexcarantona <info@alejandrocarantonna.es>
2021-12-26 03:00:02 +0100

    #3.21 & #3.22 Deployment and linting
    The app now works on Heroku, MongoDBAtlas and is linted to be correctly 
    formatted

--- index.js
+++ index.js
@@ -15,62 +15,62 @@ const Person = require('./models/Person');
 
 //Database connection.
 mongoose.connect(process.env.MONGODB_URI)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  });
+    .then(result => {
+        console.log('connected to MongoDB');
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message);
+    });
 
 app.get('/api/persons', (req, res) => {
-  Person.find({})
-  .then(persons => res.json(persons));
+    Person.find({})
+        .then(persons => res.json(persons));
 });
 
 app.post('/api/persons', (req, res, next) => {
-  const newNote = new Person(req.body);
-  newNote.save(newNote)
-  .then(response => res.json(newNote))
-  .catch(error => next(error));
+    const newNote = new Person(req.body);
+    newNote.save(newNote)
+        .then(response => res.json(newNote))
+        .catch(error => next(error));
 });
 
 app.get('/api/persons/:id', (req, res, next) => {
-  Person.findById(req.params.id)
-  .then(person => {
-    if (person) res.json(person);
-    else res.status(404).end()
-  })
-  .catch(error => next(error))
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) res.json(person);
+            else res.status(404).end();
+        })
+        .catch(error => next(error));
 });
 
 app.delete('/api/persons/:id', (req, res) => {
-  Person.findByIdAndRemove(req.params.id)
-  .then(r => res.status(204).end());
+    Person.findByIdAndRemove(req.params.id)
+        .then(r => res.status(204).end());
 });
 
 app.put('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndUpdate(req.params.id,
-    {number: req.body.number},
-    {new: true,
-    runValidators: true})
-  .then(r => res.json(r))
-  .catch(error => next(error))
+    Person.findByIdAndUpdate(req.params.id,
+        {number: req.body.number},
+        {new: true,
+            runValidators: true})
+        .then(r => res.json(r))
+        .catch(error => next(error));
 });
 
 
 // Error handling.
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message);
+    console.error(error.message);
 
-  if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'Malformatted id'})
-  }
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'Malformatted id'});
+    }
 
-  if (error.name === 'ValidationError') {
-    return res.status(400).send({ error: error.message})
-  }
-}
+    if (error.name === 'ValidationError') {
+        return res.status(400).send({ error: error.message});
+    }
+};
 app.use(errorHandler);
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}...`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
