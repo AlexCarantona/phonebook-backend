ae143b4d2eaf7a3684bd2f21e96f9cd71f1481b5
alexcarantona <info@alejandrocarantonna.es>
2021-12-26 03:08:45 +0100

    uiBuild

--- models/Person.js
+++ models/Person.js
@@ -2,25 +2,25 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const personSchema = mongoose.Schema({
-  name: {
-    type: String,
-    minLength: 3,
-    required: true,
-    unique: true
-  },
-  number: {
-    type: String,
-    minLength: 8,
-    required: true
-  }
+    name: {
+        type: String,
+        minLength: 3,
+        required: true,
+        unique: true
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        required: true
+    }
 });
 
 personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  }
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
 });
 
 personSchema.plugin(uniqueValidator);
